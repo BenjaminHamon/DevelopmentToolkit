c2cbdf0a30ceba86bf7b8d61e2fc88c22d2d27c8
Benjamin Hamon <development@benjaminhamon.com>
2024-09-09 15:15:10 +0200

    Refactor most python helpers into a python environment class

---
+++ Sources/toolkit/bhamon_development_toolkit/python/python_environment.py
@@ -0,0 +1,85 @@
+import logging
+import os
+import platform
+import shutil
+import sys
+from typing import List, Optional
+
+from bhamon_development_toolkit.processes import process_helpers
+from bhamon_development_toolkit.processes.executable_command import ExecutableCommand
+
+
+logger = logging.getLogger("Python")
+
+
+class PythonEnvironment:
+
+
+    def __init__(self, system_python_executable: str, venv_directory: str, extra_package_repository: Optional[str] = None) -> None:
+        self._system_python_executable = system_python_executable
+        self._venv_directory = venv_directory
+        self._extra_package_repository = extra_package_repository
+
+
+    def get_venv_directory(self) -> str:
+        return self._venv_directory
+
+
+    def get_venv_python_executable(self) -> str:
+        return self.get_venv_executable("python")
+
+
+    def get_venv_executable(self, executable: str) -> str:
+        if platform.system() == "Windows":
+            return os.path.join(self._venv_directory, "scripts", executable + ".exe")
+        return os.path.join(self._venv_directory, "bin", executable)
+
+
+    def setup_virtual_environment(self, simulate: bool = False) -> None:
+        venv_python_executable = self.get_venv_python_executable()
+        if sys.executable.lower() == os.path.abspath(venv_python_executable).lower():
+            raise RuntimeError("Active python is the target virtual environment")
+
+        if os.path.isdir(self._venv_directory) and not simulate:
+            # Try to remove the executable first since it might be in use, otherwise we would be leaving a broken venv
+            if platform.system() == "Windows" and os.path.exists(os.path.join(self._venv_directory, "scripts", "python.exe")):
+                os.remove(os.path.join(self._venv_directory, "scripts", "python.exe"))
+            shutil.rmtree(self._venv_directory)
+
+        venv_command = ExecutableCommand(self._system_python_executable)
+        venv_command.add_arguments([ "-m", "venv", self._venv_directory ])
+
+        process_helpers.run_simple(logger, venv_command, simulate = simulate)
+        self.install_python_packages([ "pip", "wheel" ], simulate = simulate)
+
+
+    def install_python_packages(self,
+            name_or_path_collection: List[str], simulate: bool = False) -> None:
+
+        install_command = ExecutableCommand(self.get_venv_python_executable())
+        install_command.add_arguments([ "-m", "pip", "install", "--upgrade" ])
+
+        if self._extra_package_repository is not None:
+            install_command.add_arguments([ "--extra-index", self._extra_package_repository ])
+
+        install_command.add_arguments(name_or_path_collection)
+
+        process_helpers.run_simple(logger, install_command, simulate = simulate)
+
+
+    def install_python_packages_for_development(self,
+            name_or_path_collection: List[str], simulate: bool = False) -> None:
+
+        def is_local_package(name_or_path: str) -> bool:
+            return name_or_path.startswith(".") or "/" in name_or_path or "\\" in name_or_path
+
+        install_command = ExecutableCommand(self.get_venv_python_executable())
+        install_command.add_arguments([ "-m", "pip", "install", "--upgrade" ])
+
+        if self._extra_package_repository is not None:
+            install_command.add_arguments([ "--extra-index", self._extra_package_repository ])
+
+        for name_or_path in name_or_path_collection:
+            install_command.add_arguments([ "--editable", name_or_path ] if is_local_package(name_or_path) else [ name_or_path ])
+
+        process_helpers.run_simple(logger, install_command, simulate = simulate)
---
+++ Tests/toolkit/bhamon_development_toolkit_tests/python/test_python_environment_with_integration.py
@@ -0,0 +1,58 @@
+""" Integration tests for PythonEnvironment """
+
+import os
+
+import pytest
+
+from bhamon_development_toolkit.processes.exceptions.process_failure_exception import ProcessFailureException
+from bhamon_development_toolkit.python import python_helpers
+from bhamon_development_toolkit.python.python_environment import PythonEnvironment
+
+
+def test_setup_virtual_environment(tmpdir):
+    python_environment = PythonEnvironment(
+        system_python_executable = python_helpers.resolve_system_python_executable(),
+        venv_directory = os.path.join(tmpdir, "Working", ".venv"),
+    )
+
+    python_environment.setup_virtual_environment()
+
+    assert os.path.exists(python_environment.get_venv_directory())
+    assert os.path.exists(python_environment.get_venv_python_executable())
+
+
+def test_install_python_packages(tmpdir):
+    python_environment = PythonEnvironment(
+        system_python_executable = python_helpers.resolve_system_python_executable(),
+        venv_directory = os.path.join(tmpdir, "Working", ".venv"),
+    )
+
+    python_environment.setup_virtual_environment()
+
+    python_environment.install_python_packages([ "pip" ])
+    with pytest.raises(ProcessFailureException):
+        python_environment.install_python_packages([ "does-not-exist" ])
+
+
+def test_install_python_packages_for_development(tmpdir):
+    python_environment = PythonEnvironment(
+        system_python_executable = python_helpers.resolve_system_python_executable(),
+        venv_directory = os.path.join(tmpdir, "Working", ".venv"),
+    )
+
+    local_package_path = os.path.join(tmpdir, "Working", "Sources", "my_package")
+
+    python_environment.setup_virtual_environment()
+
+    with pytest.raises(ProcessFailureException):
+        python_environment.install_python_packages_for_development([ local_package_path ])
+
+    os.makedirs(local_package_path)
+    with open(os.path.join(local_package_path, "pyproject.toml"), mode = "w", encoding = "utf-8") as pyproject_file:
+        pyproject_file.write("[project]\n")
+        pyproject_file.write("name = \"my-local-package\"\n")
+        pyproject_file.write("version = \"1.0\"\n")
+
+    python_environment.install_python_packages_for_development([ local_package_path ])
+
+    assert os.path.exists(os.path.join(local_package_path, "my_local_package.egg-info"))
