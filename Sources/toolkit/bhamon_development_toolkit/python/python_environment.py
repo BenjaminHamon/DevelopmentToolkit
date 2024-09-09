3945c20e67970df9d072fd0c9f89ef3c89b11ba2
Benjamin Hamon <development@benjaminhamon.com>
2024-09-09 15:20:02 +0200

    Use pip configuration file instead of passing index argument

--- Sources/toolkit/bhamon_development_toolkit/python/python_environment.py
+++ Sources/toolkit/bhamon_development_toolkit/python/python_environment.py
@@ -15,10 +15,9 @@ logger = logging.getLogger("Python")
 class PythonEnvironment:
 
 
-    def __init__(self, system_python_executable: str, venv_directory: str, extra_package_repository: Optional[str] = None) -> None:
+    def __init__(self, system_python_executable: str, venv_directory: str) -> None:
         self._system_python_executable = system_python_executable
         self._venv_directory = venv_directory
-        self._extra_package_repository = extra_package_repository
 
 
     def get_venv_directory(self) -> str:
@@ -35,7 +34,13 @@ class PythonEnvironment:
         return os.path.join(self._venv_directory, "bin", executable)
 
 
-    def setup_virtual_environment(self, simulate: bool = False) -> None:
+    def _get_pip_configuration_file_path(self) -> str:
+        if platform.system() == "Windows":
+            return os.path.join(self._venv_directory, "pip.ini")
+        return os.path.join(self._venv_directory, "pip.conf")
+
+
+    def setup_virtual_environment(self, pip_configuration_file_path: Optional[str] = None, simulate: bool = False) -> None:
         venv_python_executable = self.get_venv_python_executable()
         if sys.executable.lower() == os.path.abspath(venv_python_executable).lower():
             raise RuntimeError("Active python is the target virtual environment")
@@ -50,6 +55,12 @@ class PythonEnvironment:
         venv_command.add_arguments([ "-m", "venv", self._venv_directory ])
 
         process_helpers.run_simple(logger, venv_command, simulate = simulate)
+
+        if pip_configuration_file_path is not None:
+            pip_configuration_file_path_in_venv = self._get_pip_configuration_file_path()
+            if not simulate:
+                shutil.copy(pip_configuration_file_path, pip_configuration_file_path_in_venv)
+
         self.install_python_packages([ "pip", "wheel" ], simulate = simulate)
 
 
@@ -57,12 +68,7 @@ class PythonEnvironment:
             name_or_path_collection: List[str], simulate: bool = False) -> None:
 
         install_command = ExecutableCommand(self.get_venv_python_executable())
-        install_command.add_arguments([ "-m", "pip", "install", "--upgrade" ])
-
-        if self._extra_package_repository is not None:
-            install_command.add_arguments([ "--extra-index", self._extra_package_repository ])
-
-        install_command.add_arguments(name_or_path_collection)
+        install_command.add_arguments([ "-m", "pip", "install", "--upgrade" ] + name_or_path_collection)
 
         process_helpers.run_simple(logger, install_command, simulate = simulate)
 
@@ -75,9 +81,6 @@ class PythonEnvironment:
 
         install_command = ExecutableCommand(self.get_venv_python_executable())
         install_command.add_arguments([ "-m", "pip", "install", "--upgrade" ])
-
-        if self._extra_package_repository is not None:
-            install_command.add_arguments([ "--extra-index", self._extra_package_repository ])
 
         for name_or_path in name_or_path_collection:
             install_command.add_arguments([ "--editable", name_or_path ] if is_local_package(name_or_path) else [ name_or_path ])
