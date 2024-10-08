c5f6fed1d70c8b9fe46514ac3363026c21348fcb
Benjamin Hamon <development@benjaminhamon.com>
2024-10-08 10:50:39 +0200

    Suppress warnings about missing metadata to support classic workflow

--- Sources/toolkit/bhamon_development_toolkit/__init__.py
+++ Sources/toolkit/bhamon_development_toolkit/__init__.py
@@ -5,12 +5,14 @@ __date__ = None
 
 
 try:
-    import bhamon_development_toolkit.__metadata__
+    import bhamon_development_toolkit.__metadata__ # type: ignore
 
+    # pylint: disable = no-member
     __product__ = bhamon_development_toolkit.__metadata__.__product__
     __copyright__ = bhamon_development_toolkit.__metadata__.__copyright__
     __version__ = bhamon_development_toolkit.__metadata__.__version__
     __date__ = bhamon_development_toolkit.__metadata__.__date__
+    # pylint: enable = no-member
 
 except ImportError:
     pass
